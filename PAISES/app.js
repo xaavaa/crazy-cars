8a69fb3aff2fc79f3ffac7ef19cb44dd37b3ed64
xaavaa <xaviercanalsala@gmail.com>
2022-10-15 22:21:14 +0200

    add videos, playVideo()

---
+++ PAISES/app.js
@@ -0,0 +1,330 @@
+//This will use the demo backend if you open index.html locally via file://, otherwise your server will be used
+let backendUrl = location.protocol === 'file:' ? "https://tiktok-chat-reader.zerody.one/" : undefined;
+let connection = new TikTokIOConnection(backendUrl);
+
+// Counter
+let viewerCount = 0;
+let likeCount = 0;
+let diamondsCount = 0;
+
+// These settings are defined by obs.html
+if (!window.settings) window.settings = {};
+
+$(document).ready(() => {
+    $('#connectButton').click(connect);
+    $('#stopButton').click(endConnection);
+    $('#uniqueIdInput').on('keyup', function (e) {
+        if (e.key === 'Enter') {
+            connect();
+        }
+    });
+
+    if (window.settings.username) connect();
+
+    var dict = {
+        alemania: 0,
+        arabia: 0,
+        china: 0,
+        eeuu : 0,
+        espana: 0,
+        francia: 0,
+        india: 0,
+        inglaterra: 0,
+        ucrania: 0
+      };
+
+    
+})
+var tempLikes = 0;
+function connect() {
+    let uniqueId = window.settings.username || $('#uniqueIdInput').val();
+    if (uniqueId !== '') {
+
+        $('#stateText').text('Connecting...');
+
+        connection.connect(uniqueId, {
+            enableExtendedGiftInfo: true
+        }).then(state => {
+            // reset stats
+            viewerCount = 0;
+            likeCount = 0;
+            diamondsCount = 0;
+            updateRoomStats();
+
+        }).catch(errorMessage => {
+            $('#stateText').text(errorMessage);
+
+            // schedule next try if obs username set
+            if (window.settings.username) {
+                setTimeout(() => {
+                    connect(window.settings.username);
+                }, 30000);
+            }
+        })
+
+    } else {
+        alert('no username entered');
+    }
+}
+
+// Prevent Cross site scripting (XSS)
+function sanitize(text) {
+    return text.replace(/</g, '&lt;')
+}
+
+function updateRoomStats() {
+    $('#roomStats').html(`Viewers: <b>${viewerCount.toLocaleString()}</b> Likes: <b>${likeCount.toLocaleString()}</b> Earned Diamonds: <b>${diamondsCount.toLocaleString()}</b>`)
+}
+
+function generateUsernameLink(data) {
+    return `<a class="usernamelink" href="https://www.tiktok.com/@${data.uniqueId}" target="_blank">${data.uniqueId}</a>`;
+}
+
+function isPendingStreak(data) {
+    return data.giftType === 1 && !data.repeatEnd;
+}
+
+async function playVideo(src) {
+    console.log("playvideo")
+    let videoElem = document.getElementById("video");
+
+    videoElem.setAttribute('src', src);
+
+    try {
+      await videoElem.play();
+      console.log("playing");
+    } catch(err) {
+        console.log("not playing, error: "+err);
+    }
+  }
+
+function addLikesItem(data) {
+    let container = location.href.includes('obs.html') ? $('.eventcontainer') : $('.likescontainer');
+    window.tempLikes += data.likeCount;
+    console.log(tempLikes);
+    
+    if (window.tempLikes > 100) {
+        console.log("hasbullah")
+        var i = Math.floor(Math.random() * 2) + 1;
+
+        playVideo("videos/hasbullah/"+i+".mp4");
+        tempLikes = 0;
+    }
+
+    if (container.find('div').length > 200) {
+        container.find('div').slice(0, 100).remove();
+    }
+
+
+    /*
+    if(data.isSubscriber) {
+        var seguidor = "Seguidor";
+    } else {
+        var seguidor = "No Seguidor";
+    } 
+    
+    let streakId = data.nickname.toString() + '_' + data.likeCount;
+
+    let html = `
+        <div data-streakid=${isPendingStreak(data) ? streakId : ''}>
+            <img class="miniprofilepicture" src="${data.profilePictureUrl}">
+            <span>
+                <b>${generateUsernameLink(data)}:</b> <span>${seguidor}</span><br>
+                <div>
+                    <table>
+                        <tr>
+                            <td>
+                                <span>Likes: <b>${data.likeCount}</b> (ID:${data.userId})<span><br>
+                            </td>
+                        </tr>
+                    </tabl>
+                </div>
+            </span>
+        </div>
+    `; 
+
+    let existingStreakItem = container.find(`[data-streakid='${streakId}']`);
+
+    if (existingStreakItem.length) {
+        existingStreakItem.replaceWith(html);
+    } else {
+        container.append(html);
+    }
+
+    container.stop();
+    container.animate({
+        scrollTop: container[0].scrollHeight
+    }, 800); */
+}
+
+/**
+ * Add a new message to the chat container
+ */
+function addChatItem(color, data, text, summarize) {
+    let container = location.href.includes('obs.html') ? $('.eventcontainer') : $('.chatcontainer');
+
+    if (container.find('div').length > 500) {
+        container.find('div').slice(0, 200).remove();
+    }
+/*
+    container.find('.temporary').remove();;
+
+    container.append(`
+        <div class=${summarize ? 'temporary' : 'static'}>
+            <img class="miniprofilepicture" src="${data.profilePictureUrl}">
+            <span>
+                <b>${generateUsernameLink(data)}:</b> 
+                <span style="color:${color}">${sanitize(text)}</span>
+            </span>
+        </div>
+    `);
+
+    container.stop();
+    container.animate({
+        scrollTop: container[0].scrollHeight
+    }, 400); */
+}
+
+/**
+ * Add a new gift to the gift container
+ */
+function addGiftItem(data) {
+    let container = location.href.includes('obs.html') ? $('.eventcontainer') : $('.giftcontainer');
+
+    if (container.find('div').length > 200) {
+        container.find('div').slice(0, 100).remove();
+    }
+
+    let streakId = data.userId.toString() + '_' + data.giftId;
+
+    if (data.giftPictureUrl == "https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/eba3a9bb85c33e017f3648eaf88d7189~tplv-obj.webp"){
+        console.log("spain")
+        var i = Math.floor(Math.random() * 5) + 1;
+        playVideo("videos/spain/"+i+".mp4");
+        
+    } else if (data.giftPictureUrl == "https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/a99fc8541c7b91305de1cdcf47714d03~tplv-obj.webp") {
+        console.log("spain")
+        var i = Math.floor(Math.random() * 4) + 1;
+        playVideo("videos/india/"+i+".mp4");
+
+    }
+/*
+    let html = `
+        <div data-streakid=${isPendingStreak(data) ? streakId : ''}>
+            <img class="miniprofilepicture" src="${data.profilePictureUrl}">
+            <span>
+                <b>${generateUsernameLink(data)}:</b> <span>${data.describe}</span><br>
+                <div>
+                    <table>
+                        <tr>
+                            <td><img class="gifticon" src="${data.giftPictureUrl}"></td>
+                            <td>
+                                <span>Name: <b>${data.giftName}</b> (ID:${data.giftId})<span><br>
+                                <span>Repeat: <b style="${isPendingStreak(data) ? 'color:red' : ''}">x${data.repeatCount.toLocaleString()}</b><span><br>
+                                <span>Cost: <b>${(data.diamondCount * data.repeatCount).toLocaleString()} Diamonds</b><span>
+                            </td>
+                        </tr>
+                    </tabl>
+                </div>
+            </span>
+        </div>
+    `;
+
+    let existingStreakItem = container.find(`[data-streakid='${streakId}']`);
+
+    if (existingStreakItem.length) {
+        existingStreakItem.replaceWith(html);
+    } else {
+        container.append(html);
+    }
+
+    container.stop();
+    container.animate({
+        scrollTop: container[0].scrollHeight
+    }, 800); */
+}
+
+
+// viewer stats
+connection.on('roomUser', (msg) => {
+    if (typeof msg.viewerCount === 'number') {
+        viewerCount = msg.viewerCount;
+        updateRoomStats();
+    }
+})
+
+// like stats
+connection.on('like', (msg) => {
+    if (typeof msg.totalLikeCount === 'number') {
+        likeCount = msg.totalLikeCount;
+        updateRoomStats();
+    }
+
+    if (window.settings.showLikes === "0") return;
+
+    if (typeof msg.likeCount === 'number') {
+        addChatItem('#447dd4', msg, msg.label.replace('{0:user}', '').replace('likes', `${msg.likeCount} likes`))
+    }
+
+    addLikesItem(msg);
+
+})
+
+// Member join
+let joinMsgDelay = 0;
+connection.on('member', (msg) => {
+    if (window.settings.showJoins === "0") return;
+
+    let addDelay = 250;
+    if (joinMsgDelay > 500) addDelay = 100;
+    if (joinMsgDelay > 1000) addDelay = 0;
+
+    joinMsgDelay += addDelay;
+
+    setTimeout(() => {
+        joinMsgDelay -= addDelay;
+        addChatItem('#21b2c2', msg, 'joined', true);
+    }, joinMsgDelay);
+})
+
+// New chat comment received
+connection.on('chat', (msg) => {
+    if (window.settings.showChats === "0") return;
+
+    addChatItem('', msg, msg.comment);
+})
+
+// New gift received
+connection.on('gift', (data) => {
+    if (!isPendingStreak(data) && data.diamondCount > 0) {
+        diamondsCount += (data.diamondCount * data.repeatCount);
+        updateRoomStats();
+    }
+
+    if (window.settings.showGifts === "0") return;
+
+    addGiftItem(data);
+})
+
+// share, follow
+connection.on('social', (data) => {
+    if (window.settings.showFollows === "0") return;
+
+    let color = data.displayType.includes('follow') ? '#ff005e' : '#2fb816';
+    addChatItem(color, data, data.label.replace('{0:user}', ''));
+})
+
+connection.on('streamEnd', () => {
+    $('#stateText').text('Stream ended.');
+
+    // schedule next try if obs username set
+    if (window.settings.username) {
+        setTimeout(() => {
+            connect(window.settings.username);
+        }, 30000);
+    }
+})
+
+function endConnection() {
+    document.location.reload(true);
+}
