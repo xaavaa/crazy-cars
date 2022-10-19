a177cac446fce4c8061b707597e5393728f1c978
xaavaa <xaviercanalsala@gmail.com>
2022-10-19 16:48:59 +0200

    a

--- PAISES/app.js
+++ PAISES/app.js
@@ -319,10 +319,37 @@ function addChatItem(color, data, text, summarize) {
  */
 
 function sortTops(){
+    var items = Object.keys(window.dictTops).map(
+        (key) => { return [key, dict[key]] });
 
+    items.sort(
+        (first, second) => { return first[1] - second[1] }
+    );
+
+    window.dictTops = items.map(
+        (e) => { return e[0] });
 }
 
-function updateDictionaryTops() {
+function showTops() {
+    var top1 = document.getElementById('top1_text');
+    var top2 = document.getElementById('top2_text');
+    var top3 = document.getElementById('top3_text');
+    var top4 = document.getElementById('top4_text');
+    var top5 = document.getElementById('top5_text');
+    var top_img = document.getElementsByClassName('tops');
+
+    top1 = Object.values(window.dictTops)[0];
+    top2 = Object.values(window.dictTops)[2];
+    top3 = Object.values(window.dictTops)[3];
+    top4 = Object.values(window.dictTops)[4];
+    top5 = Object.values(window.dictTops)[5];
+
+    top_img[0].setAttribute('src', src) = dictImages.Object.keys(window.dictTops)[0];
+    top_img[1].setAttribute('src', src) = dictImages.Object.keys(window.dictTops)[1];
+    top_img[2].setAttribute('src', src) = dictImages.Object.keys(window.dictTops)[2];
+    top_img[3].setAttribute('src', src) = dictImages.Object.keys(window.dictTops)[3];
+    top_img[4].setAttribute('src', src) = dictImages.Object.keys(window.dictTops)[4];
+    
 
 }
 
@@ -338,6 +365,18 @@ var dictTops = {
     arabia : 0 , 
   }
 
+const dictImages = { 
+    usa : "imgs/eeuu.png" , 
+    india : "imgs/india.png" , 
+    ukraina : "imgs/ucraina.png" , 
+    spain : "imgs/espana.png" , 
+    uk : "imgs/inglaterra.png" , 
+    china : "imgs/china.png" , 
+    francia : "imgs/francia.png" , 
+    alemania : "imgs/alemania.png" , 
+    arabia : "imgs/arabia.png" , 
+  }
+
 
 function addGiftItem(data) {
     let container = location.href.includes('obs.html') ? $('.eventcontainer') : $('.giftcontainer');
@@ -351,44 +390,55 @@ function addGiftItem(data) {
     if (data.giftPictureUrl == "https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/eba3a9bb85c33e017f3648eaf88d7189~tplv-obj.png"){
         console.log("usa")
         var i = Math.floor(Math.random() * 2) + 1;
+        window.dictTops["usa"] = window.dictTops["usa"] + data.repeatCount;
+        sortTops();
+        showTops();
         playVideo("videos/usa/"+i+".mp4",data.repeatCount,"usa");
         
     } else if (data.giftPictureUrl == "https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/a99fc8541c7b91305de1cdcf47714d03~tplv-obj.png") {
         console.log("india")
         var i = Math.floor(Math.random() * 4) + 1;
+        window.dictTops["india"] = window.dictTops["india"] + data.repeatCount;
         playVideo("videos/india/"+i+".mp4",data.repeatCount,"india");
 
     } else if (data.giftPictureUrl == "https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/802a21ae29f9fae5abe3693de9f874bd~tplv-obj.png") {
         console.log("ukraina")            
         var i = Math.floor(Math.random() * 2) + 1;
+        window.dictTops["ukraina"] = window.dictTops["ukraina"] + data.repeatCount;
         playVideo("videos/ukraina/"+i+".mp4",data.repeatCount,"ukraina");
     } else if (data.giftPictureUrl == "https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/968820bc85e274713c795a6aef3f7c67~tplv-obj.png") {
         console.log("spain")            
         var i = Math.floor(Math.random() * 5) + 1;
+        window.dictTops["spain"] = window.dictTops["spain"] + data.repeatCount;
         playVideo("videos/spain/"+i+".mp4",data.repeatCount,"spain");
     } else if (data.giftPictureUrl == "https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/09d9b188294ecf9b210c06f4e984a3bd~tplv-obj.png") {
         console.log("uk")            
         var i = Math.floor(Math.random() * 1) + 1;
+        window.dictTops["uk"] = window.dictTops["uk"] + data.repeatCount;
         playVideo("videos/uk/"+i+".mp4",data.repeatCount,"uk");
     }
     else if (data.giftPictureUrl == "https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/728cc7436005cace2791aa7500e4bf95~tplv-obj.png") {
         console.log("china")            
         var i = Math.floor(Math.random() * 2) + 1;
+        window.dictTops["china"] = window.dictTops["china"] + data.repeatCount;
         playVideo("videos/china/"+i+".mp4",data.repeatCount,"china");
     }
     else if (data.giftPictureUrl == "https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/c043cd9e418f13017793ddf6e0c6ee99~tplv-obj.png") {
         console.log("france")            
         var i = Math.floor(Math.random() * 2) + 1;
+        window.dictTops["france"] = window.dictTops["france"] + data.repeatCount;
         playVideo("videos/france/"+i+".mp4",data.repeatCount,"france");
     }
     else if (data.giftPictureUrl == "https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/3f02fa9594bd1495ff4e8aa5ae265eef~tplv-obj.png") {
         console.log("alemania")            
         var i = Math.floor(Math.random() * 1) + 1;
+        window.dictTops["alemania"] = window.dictTops["alemania"] + data.repeatCount;
         playVideo("videos/alemania/"+i+".mp4",data.repeatCount,"alemania");
     }
     else if (data.giftPictureUrl == "https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/a43ec3a70f63d2d48683bed39e18cd2d~tplv-obj.png") {
         console.log("arabia")            
         var i = Math.floor(Math.random() * 2) + 1;
+        window.dictTops["arabia"] = window.dictTops["arabia"] + data.repeatCount;
         playVideo("videos/arabia/"+i+".mp4",data.repeatCount,"arabia");
     }
 /*
